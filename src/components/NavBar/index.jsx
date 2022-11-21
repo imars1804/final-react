48e41ea04395651d2cd2c0348f82484b137e5dd3
Laureana Lupinacci <lalilupinacci@gmail.com>
2022-11-21 17:26:56 -0300

    Viendo si el Navbar está bien linkeado

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const NavBar = () => {
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav_brand">
+                    <a className="nav_link" href="#"> Mi-Marca </a>
+                </div>
+                <ul className="nav_list">
+                    <li>
+                        <a className="nav_link" href="#"> Categoria 1 </a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#"> Categoria 2 </a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#"> Categoria 3 </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
